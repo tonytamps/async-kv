b314556dc25e029ea7a7ca3557f07886999d4102
Brendan Jefferis <brendan.jefferis@gmail.com>
2022-01-04 16:01:03 +1100

    Revert "Adds unit tests for `all`"
    This reverts commit 9af0b2513ff00f33e5634dd2349933a48d009e7c.

--- test/all.test.ts
+++ test/all.test.ts
@@ -1,48 +1,5 @@
 import { all } from "../src";
 
-const delayedAsync = async (val: any, delay: number) =>
-  new Promise((res) => setTimeout(() => res(val), delay));
-
-describe("all", () => {
-  it("should return multiple keyed promises as object", async () => {
-    const result = await all({
-      first: () => Promise.resolve(1),
-      second: () => Promise.resolve(2),
-    });
-    expect(result.first).toBe(1);
-    expect(result.second).toBe(2);
-  });
-
-  it("should execute promises in parallel", async () => {
-    const DELAY = 10;
-
-    const start = Date.now();
-    await all({
-      first: () => delayedAsync(1, DELAY),
-      second: () => delayedAsync(2, DELAY),
-    });
-    const end = Date.now() - start;
-
-    expect(end).toBeLessThan(20);
-  });
-
-  it("should retain object order", async () => {
-    const result = await all({
-      first: () => delayedAsync(1, 10),
-      second: () => Promise.resolve(2),
-    });
-
-    expect(Object.keys(result)).toEqual(["first", "second"]);
-  });
-
-  it("should fail with error if any promise fails", async () => {
-    try {
-      await all({
-        first: () => Promise.reject("First failed"),
-        second: () => Promise.resolve(2),
-      });
-    } catch (e) {
-      expect(e).toBe("First failed");
-    }
-  });
+test("all is a function", () => {
+  expect(typeof all).toBe("function");
 });
