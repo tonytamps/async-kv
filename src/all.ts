8c02b930acce3d4de320f2c06d0130886f2994fe
Tony Tamplin <github@tony.dev>
2022-01-05 17:46:44 +1300

    Move types into the file they're needed in to they're properly included in the build.

--- src/all.ts
+++ src/all.ts
@@ -1,3 +1,10 @@
+type UnwrapPromise<T> = T extends PromiseLike<infer U> ? U : T;
+
+type FunctionDict = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: (...args: any[]) => any;
+};
+
 export async function all<T extends FunctionDict>(
     iterable: T
 ): Promise<{ [K in keyof T]: UnwrapPromise<ReturnType<T[K]>> }> {
